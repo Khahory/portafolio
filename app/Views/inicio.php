548181cb9a38c7d7183c96f33fd007c7b46692e1
Khahory <angeld05@live.com>
2020-09-23 03:48:44 +0200

    go

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -1,6 +1,9 @@
 <div id="app">
 
+    <img class="thumbnail" src="https://simpleparallax.b-cdn.net/images/paint_1.jpg" alt="image">
+
     <section class="hero is-success is-fullheight has-bg-img">
+        Hello
         <div class="hero-body">
             <div class="container has-text-centered">
                 <h1 class="title">
@@ -13,9 +16,18 @@
         </div>
     </section>
 
+
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
+
 <script>
+    const simpleParallax = require('simple-parallax-js');
+    var image = document.getElementsByClassName('thumbnail');
+    new simpleParallax(image, {
+        overflow: true
+    });
+
     new WOW().init();
     const app = new Vue({
         el: '#app',
